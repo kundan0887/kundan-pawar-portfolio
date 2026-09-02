7d7241f81ed4843ac5c8164bc6cdaef120228697
Kundan <kundanpawar2987@gmail.com>
2026-09-02 14:29:19 +0200

    chore: organize project structure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -28,7 +28,7 @@ export function truncateText(text: string, maxLength: number): string {
 }
 
 export function generateId(): string {
-  return Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36).substr(2, 9); 
 }
 
 export function debounce<T extends (...args: unknown[]) => unknown>(
